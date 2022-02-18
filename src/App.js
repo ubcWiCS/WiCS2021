6cad1047465f33b57a6e0a5809a66dd127d24623
emsandrews <74979194+emsandrews@users.noreply.github.com>
2022-02-17 16:51:49 -0800

    website update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Post from "./components/Post";
+import SinglePost from "./components/SinglePost";
+import SinglePostNoAuthor from "./components/SinglePostNoAuthor";
+import Team from "./components/Team";
+import Advisors from "./components/Advisors";
+import Sponsors from "./components/Sponsors";
+import SponsorsTest from "./components/SponsorTest";
+import Events from "./components/Events";
+import NavBar from "./components/NavBar";
+import SinglePostSponsor from "./components/SinglePostSponsor";
+import TeamTest from "./components/TeamTest";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={SinglePost} path="/post/:slug" />
+        <Route component={SinglePostNoAuthor} path="/profile/:slug" />
+        <Route component={Post} path="/post" />
+        <Route component={TeamTest} path="/team" />
+        <Route component={Advisors} path="/advisors" />
+        <Route component={Sponsors} path="/sponsors" />
+        <Route component={SinglePostSponsor} path="/sponsor/:slug" />
+        <Route component={Events} path="/events" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
