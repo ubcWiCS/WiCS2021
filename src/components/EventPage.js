749f6a8b6398b3e5b7ed6f6f2be013e6461202f0
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2022-10-23 10:40:48 -0700

    Events page (#4)
    * add event page component and routing
    
    * add past events to events page
    
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>

---
+++ src/components/EventPage.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client.js";
+import BlockContent from "@sanity/block-content-to-react";
+
+import Footer from "./Footer.js";
+import EventContent from "./EventContent";
+
+export default function EventPage() {
+    const [postData, setPost] = useState(null);
+    useEffect(() => {
+      sanityClient
+        .fetch(
+          `*[_type == "events"] | order(pageOrder asc){
+        title,
+        date,
+        direction,
+        body,
+        pageOrder,
+        page,
+        orderOnPage,
+        mainImage{
+          asset->{
+            _id,
+            url
+          },
+          alt
+        }
+      }`
+        )
+        .then((data) => setPost(data))
+        .catch(console.error);
+    }, []);
+
+  return (
+    <main>
+         <h1 className="text-5xl flex justify-center cursive text-gray-700 title">
+         Past Events
+        </h1>
+          {postData &&
+          postData.map((event) => (
+            <section key={event.pageOrder}>
+              <EventContent
+                title={event.title}
+                body={
+                  <BlockContent
+                    blocks={event.body}
+                    projectId="xvhe4elt"
+                    dataset="production"
+                  />
+                }
+                date={event.date}
+                imageSrc={event.mainImage.asset.url}
+                imageAlt={event.mainImage.alt}
+                direction={event.direction}
+             />
+            </section>
+          ))}
+        <Footer></Footer>
+    </main>
+  );
+}
+
