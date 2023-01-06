ed2287d7075c0c88b9b826f8560d42c93da33673
Imogene McDonald <61811702+imogenem@users.noreply.github.com>
2023-01-05 16:24:44 -0800

    new navbar pages (#10)
    Co-authored-by: Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Footer from "../components/navigation/Footer.js";
+
+export default function About() {
+  return (
+    <main>
+      <h1 className="text-5xl flex justify-center cursive text-gray-700 title">
+        About
+      </h1>
+      <Footer></Footer>
+    </main>
+  );
+}
