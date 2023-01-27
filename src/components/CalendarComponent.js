ebc41b88a872c77c9eb6ca6f41370868f78dd536
Imogene Mcdonald <imogenemcdonald@Imogenes-MBP.hitronhub.home>
2023-01-26 20:13:57 -0800

    add calendar with clickable events

--- src/components/CalendarComponent.js
+++ src/components/CalendarComponent.js
@@ -1,37 +1,14 @@
-import Calendar from "@ericz1803/react-google-calendar";
-import "dotenv/config";
-import { css } from "@emotion/react";
-
-const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_CALENDAR_API;
-
-let calendars = [
-  { calendarId: "18qb8b2du4h0brqc9i7tv4v8ok@group.calendar.google.com" },
-];
-let styles = {
-  //you can use object styles (no import required)
-  calendar: {
-    borderRadius: "10px",
-    boxShadow: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-    color: "#818cf8",
-    today: css`
-      /* highlight today by making the text red and giving it a red border */
-      color: "#818cf8";
-      border: 1px solid "#818cf8";
-    `,
-  },
-};
-
 export default function CalendarComponent() {
   return (
     <main>
       <div className="w-full">
         <div className="md:px-28 px-16 pb-10 max-w-6xl block ml-auto mr-auto">
-          <Calendar
-            apiKey={GOOGLE_API_KEY}
-            calendars={calendars}
-            styles={styles}
-            showFooter={false}
-          />
+          <iframe
+            title="google-cal"
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FVancouver&showTz=0&showPrint=0&showCalendars=0&showNav=1&src=MThxYjhiMmR1NGgwYnJxYzlpN3R2NHY4b2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23cc89b6"
+            width="800"
+            height="600"
+          ></iframe>
         </div>
       </div>
     </main>
