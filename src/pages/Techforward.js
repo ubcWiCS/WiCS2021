35f47a0220a5177f578e09bf44569e28083853c3
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-11-24 22:35:23 -0800

    fix styles for smaller screens

--- src/pages/Techforward.js
+++ src/pages/Techforward.js
@@ -44,8 +44,8 @@ export default function Techforward() {
 
   return (
     <div>
-      <main className="text-center bg-purple-200 px-40">
-        <nav className="fixed left-8 top-1/2 transform -translate-y-1/2 space-y-4 z-50">
+      <main className="text-center bg-purple-200 md:px-40 px-10">
+        <nav className="fixed md:left-8 left-4 top-1/2 transform -translate-y-1/2 space-y-4 z-50">
           <a href="#hero" className="block w-4 h-4 bg-white rounded-full hover:bg-purple-700" title="Hero"></a>
           <a href="#about" className="block w-4 h-4 bg-white rounded-full hover:bg-purple-700" title="About"></a>
           <a href="#sponsors" className="block w-4 h-4 bg-white rounded-full hover:bg-purple-700" title="Sponsors"></a>
@@ -55,7 +55,7 @@ export default function Techforward() {
         </nav>
 
         <section id="hero" className="h-screen flex flex-col items-center justify-center text-center">
-          <h1 className="text-9xl font-bold text-black">TECH<span className="text-purple-600">forward</span></h1>
+          <h1 className="md:text-9xl text-5xl font-bold text-black">TECH<span className="text-purple-600">forward</span></h1>
           <p className="text-lg mt-4">bridging the gap between engineering and computer science</p>
           <button className="bg-black text-white font-bold py-2 px-6 rounded mt-6 hover:bg-gray-800">
             Register
@@ -67,9 +67,9 @@ export default function Techforward() {
           </div>
         </section>
 
-        <section id="about" className="flex pb-20 md:pl-10">
-          <div className="w-1/2">
-            <h2 className="text-6xl font-bold text-left mb-8">ABOUT</h2>
+        <section id="about" className="flex pb-20 md:pl-10 pl-3">
+          <div className="md:w-1/2">
+            <h2 className="md:text-6xl text-3xl font-bold md:text-left text-center mb-8">ABOUT</h2>
             <div className="text-left">
               <p className="text-lg">
                 TECHforward is an <span className="font-bold">in-person conference</span> aiming to bridge the gap between engineering and computer science. All <span className="font-bold">female-identifying UBC students</span> and allies interested in engineering and/or computer science are invited to attend, for the opportunity to <span className="font-bold">network</span> with professionals in various related fields!
@@ -79,14 +79,14 @@ export default function Techforward() {
         </section>
 
         <section id="sponsors" className="py-20">
-          <h2 className="text-6xl font-bold text-center mb-8">SPONSORS</h2>
+          <h2 className="md:text-6xl text-3xl font-bold text-center mb-8">SPONSORS</h2>
           <p>insert sponsor logos</p>
         </section>
 
-        <section id="schedule_speakers" className="py-20 my-20 bg-purple-100 rounded-lg">
-          <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12">
+        <section id="schedule_speakers" className="p-10 my-20 bg-purple-100 rounded-lg">
+          <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12">
             <div>
-              <h2 className="text-6xl font-bold text-center mb-8">SCHEDULE</h2>
+              <h2 className="md:text-6xl text-3xl font-bold text-center mb-8">SCHEDULE</h2>
               <div className="space-y-4">
                 {schedule.map(({time,desc,speaker_id}, idx) => {
                   return (
@@ -103,7 +103,7 @@ export default function Techforward() {
               </div>
             </div>
             <div>
-              <h2 className="text-6xl font-bold text-center mb-8">SPEAKERS</h2>
+              <h2 className="md:text-6xl text-3xl font-bold text-center mb-8">SPEAKERS</h2>
                 <div className="text-center bg-white p-10 shadow rounded">
                   {selectedSpeaker ? (
                     <div>
@@ -125,20 +125,20 @@ export default function Techforward() {
         </section>
 
         <section id="pricing" className="py-20">
-        <h2 className="text-6xl font-bold text-center mb-8">PRICING</h2>
-        <div className="flex flex-row justify-center space-x-10">
+        <h2 className="md:text-6xl text-3xl font-bold text-center mb-8">PRICING</h2>
+        <div className="flex flex-col md:flex-row justify-center gap-10 md:gap-10 align-middle">
         {prices.map(({ name, deadline, price, id }) => {
           const isPastDeadline = deadline < new Date();
           return (
             <div
               key={id}
-              className={`flex flex-col w-100 h-64 p-6 text-center justify-center rounded-md shadow-lg ${
+              className={`flex flex-col w-100 md:h-64 p-6 text-center justify-center rounded-md shadow-lg ${
                 isPastDeadline
                   ? "bg-gray-300 text-gray-500 opacity-50 cursor-not-allowed"
                   : "bg-white"
               }`}
             >
-              <h3 className="text-4xl font-bold">{name}</h3>
+              <h3 className="md:text-4xl text-2xl font-bold">{name}</h3>
               <p className="mb-4 mt-2">Purchase by {deadline.toDateString()}</p>
               <h3 className="text-3xl font-bold">${price}</h3>
             </div>
@@ -149,11 +149,11 @@ export default function Techforward() {
       </section>
       
 
-      <section id="faq" className="flex py-20">
-        <div className="w-1/3">
-          <h2 className="text-6xl font-bold text-center mb-8">FAQ</h2>
+      <section id="faq" className="md:flex py-20">
+        <div className="md:w-1/3">
+          <h2 className="md:text-6xl text-3xl font-bold text-center mb-8">FAQ</h2>
         </div>
-        <div className="w-2/3">
+        <div className="md:w-2/3">
           {faqs.map(({question,answer}, idx) => {
             return (
               <div key={idx} className="mb-10 bg-purple-100 rounded-lg p-4">
