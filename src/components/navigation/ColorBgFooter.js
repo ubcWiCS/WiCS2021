7eda1eff260d65c63cbafa075e8882f17e5b29cf
Anica1004 <anicamok@gmail.com>
2025-09-26 17:21:59 -0700

    Made the colored footer responsive

--- src/components/navigation/ColorBgFooter.js
+++ src/components/navigation/ColorBgFooter.js
@@ -17,21 +17,22 @@ export default function Footer() {
       }}
     >
       <div className=" mx-auto px-6 py-12">
-      <div className="flex flex-col md:items-start max-w-3xl mx-auto mt-20">
-      <h2 className="text-2xl font-poppins font-semibold text-wicsPurple mb-3 md:text-center md:text-left">
+      <div className="flex flex-col items-start max-w-3xl mx-auto mt-20">
+          <h2 className="text-2xl font-poppins font-semibold text-wicsPurple mb-3">
             Subscribe to Our Newsletter
           </h2>
-          <NewsletterSignupTailwind className="md:items-start text-left py-2 max-w-md" />
+          <NewsletterSignupTailwind className="items-start text-left py-2 max-w-md" />
         </div>
+
 
         <div className="border-t border-wicsPurple mt-6 mb-4 w-full max-w-3xl mx-auto"></div>
 
-        <div className="flex flex-col md:flex-row md:justify-between md:items-center max-w-3xl mx-auto w-full space-y-4 md:space-y-0">
-            <p className="text-sm font-poppins md:text-center md:text-left">
-              UBC Women in Computer Science {date}
-            </p>
-
-            <div className="flex gap-3 justify-center md:justify-end">
+   
+        <div className="flex justify-between items-center max-w-3xl mx-auto w-full">
+          <p className="text-sm mt-4 font-poppins">
+            UBC Women in Computer Science {date}
+          </p>
+          <div className="flex gap-1">
             <SocialMedia
               url="https://www.linkedin.com/company/ubcwics/mycompany/"
               width="25px"
@@ -55,6 +56,7 @@ export default function Footer() {
               width="25px"
               height="25px"
               color="#B089DD"
+              
             />
             <SocialMedia
               email="true"
