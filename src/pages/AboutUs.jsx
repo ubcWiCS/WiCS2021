1acde5f869a7b28c44b8282335f127d3b1da5032
carissaor <carissaor@gmail.com>
2026-01-09 19:55:21 -0800

    fix about us banner image

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -27,7 +27,7 @@ export default function AboutUs() {
   return (
     <>
       <main className="bg-white">
-        <div className="relative w-full h-64 md:h-96">
+        <div className="relative w-full">
           <img
             src={aboutUsBanner}
             alt="About Us Banner"
